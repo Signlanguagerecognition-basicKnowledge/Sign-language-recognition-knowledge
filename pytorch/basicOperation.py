6adb01ad82f5359bdc84d3f3f6351b584ab1c721
吴一帆 <1347933756@qq.com>
2017-12-21 12:05:15 +0800

    test
    1.添加了新的分支 Wuyfian
    2.写了第一个pytorch测试代码

---
+++ pytorch/basicOperation.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 21 12:03:39 2017
+
+@author: Administrator
+"""
+
+from __future__ import print_function
+import torch
+
+x = torch.Tensor(5, 3)
+print(x)
